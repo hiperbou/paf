d83a52d4897e4fabd20d6ce54452eaea2a4be1f6
hiperbou <hiperbou@gmail.com>
2023-05-02 15:20:56 +0200

    Update test.kt

--- src/commonTest/kotlin/test.kt
+++ src/commonTest/kotlin/test.kt
@@ -1,4 +1,4 @@
-import com.soywiz.klock.*
+/*import com.soywiz.klock.*
 import com.soywiz.korge.input.*
 import com.soywiz.korge.tests.*
 import com.soywiz.korge.tween.*
@@ -9,7 +9,7 @@ import kotlin.test.*
 
 class MyTest : ViewsForTesting() {
     @Test
-    fun test() = viewsTest {
+    fun test() = viewsTest {*/
        /* val log = arrayListOf<String>()
         val rect = solidRect(100, 100, Colors.RED)
         rect.onClick {
@@ -22,5 +22,5 @@ class MyTest : ViewsForTesting() {
         assertEquals(Rectangle(x=-102, y=0, width=100, height=100), rect.globalBounds)
         assertEquals(false, rect.isVisibleToUser())
         assertEquals(listOf("clicked"), log)*/
-    }
-}
+/*    }
+}*/
